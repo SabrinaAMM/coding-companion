9a66149798f561ffbe219b48adf8deb28f009ae2
Philipp Schiessler <philipp.schiessler@mail.de>
2020-11-23 17:49:48 +0100

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  resources :interviews, only: [:index, :show, :new, :create, :destroy] do
+    resources :bookings, only: :create
+    resources :messages, only: :create
+  end
+  resources :bookings, only: :destroy
+  resources :dashboard, only: :index
+  resources :users, only: :show
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
