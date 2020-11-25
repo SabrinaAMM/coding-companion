abbc7be7926d189545cd1299459f57d10e372aeb
Sabrina Ammerl <sammerl@gmx.de>
2020-11-25 12:04:05 +0100

    canged to Dashboard controller and view

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,9 +1,38 @@
 class DashboardController < ApplicationController
   def index
     #@user = policy_scope(User)
-    @my_interview_ids = @user.interviews.map do |interview|
-      interview.id
+    @user  =  current_user
+    @created_interviews = @user.interviews
+
+    @booked_interviews = []
+    @user.bookings.each do |booking|
+      @booked_interviews << booking.interview 
     end
-    @all_bookings = Booking.where(interview_id: @my_interview_ids)
+
+    @upcoming_interviews = []
+    @pending_interviews = []
+    @booked_interviews.each do |interview|
+      @upcoming_interviews << interview
+    end
+    @created_interview.each do |interview|
+      if interview.bookings.present? && interview.time >= DateTime.now
+        @upcoming_interviews << interview 
+      elsif interview.time >= DateTime.now
+        @pending_interviews << interview
+      end
+    end
+
+    @past_interviews = []
+    @created_interviews.each do |interview|
+      if interview.time < DateTime.now 
+        @past_interviews << interview
+      end 
+    end
+    @booked_interviews.each do |interview|
+      if interview.time < DateTime.now 
+        @past_interviews << interview
+      end 
+    end
   end
+
 end
