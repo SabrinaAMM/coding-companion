5c1154d6c7a3052219b4198e3e0a1f106f140d8a
Nicolas Poeck <Nicolaspoeck@yahoo.com>
2020-12-02 14:59:48 +0100

    Seed done

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,45 +10,34 @@ User.destroy_all
 
 puts "generating Users"
 
-image = "https://res.cloudinary.com/yowyowyow/image/upload/v1606822793/default_profil_pic_ugryyk.jpg"
-file = URI.open(image)
+john = User.create!(email: "john@example.com", password: "123456", nickname: "Johnny_the_John", bio: "I got lots of experience with jobs and I worked a lot. But now I want a new job because new jobs are good.")
+john.photo.attach(io: File.open(Rails.root.join('app','assets','images','john.jpeg')), filename: 'john.jpeg', content_type: 'image/jpeg')
 
-john = User.new(email: "john@example.com", password: "123456", nickname: "Johnny_the_John", bio: "I got lots of experience with jobs and I worked a lot. But now I want a new job because new jobs are good.")
-john.photo.attach(io: file, filename: john.nickname, content_type: 'image/jpg')
-john.save!
-file = URI.open(image)
-mary = User.new(email: "mary@example.com", password: "123456", nickname: "Cool_Mary", bio: "Hi, I am Mary and I love coding. I want to do coding as a job. For my next interview I look for cool training partners.")
-mary.photo.attach(io: file, filename: mary.nickname, content_type: 'image/jpg')
-mary.save!
-file = URI.open(image)
-zoe = User.new(email: "zoe@example.com", password: "123456", nickname: "zoe-bowie", bio: "I just finished my Computer Science degree and now I am looking for someone to practice with for a job interview.")
-zoe.photo.attach(io: file, filename: zoe.nickname, content_type: 'image/jpg')
-zoe.save!
-file = URI.open(image)
-peter = User.new(email: "peter@example.com", password: "123456", nickname: "coding-king", bio: "I am the KING OF CODING and I will show you how it works so you get every job you want!")
-peter.photo.attach(io: file, filename: peter.nickname, content_type: 'image/jpg')
-peter.save!
-file = URI.open(image)
-lisa = User.new(email: "lisa@example.com", password: "123456", nickname: "lizzy101", bio: "studied in England, worked as developer in Seoul, worked as senior develper in Tokio")
-lisa.photo.attach(io: file, filename: lisa.nickname, content_type: 'image/jpg')
-lisa.save!
-file = URI.open(image)
-bruce = User.new(email: "bruce@example.com", password: "123456", nickname: "brucybruce", bio: "I got 10 years of experience in Ruby and Ruby on rails, CSS, JS and Python. I can help you prepare for your job interview!")
-bruce.photo.attach(io: file, filename: bruce.nickname, content_type: 'image/jpg')
-bruce.save!
-file = URI.open(image)
-rob = User.new(email: "rob@example.com", password: "123456", nickname: "robbs", bio: "I got 10 years of experience in Ruby and Ruby on rails, CSS, JS and Python. I can help you prepare for your job interview!")
-rob.photo.attach(io: file, filename: rob.nickname, content_type: 'image/jpg')
-rob.save!
-file = URI.open(image)
-mario = User.new(email: "mario@example.com", password: "123456", nickname: "mast2000", bio: "I am a strategy consultant based in Munich. I just finished the Le Wagon bootcamp in Munich to learn coding and acquire the right skillset to launch my own products!")
-mario.photo.attach(io: file, filename: mario.nickname, content_type: 'image/jpg')
-mario.save!
-file = URI.open(image)
-nico = User.new(email: "nico@example.com", password: "123456", nickname: "nicolaspck", bio: "I just finished school this year and decided to learn coding because I think that it will always be useful. Coding is now my passion and I am looking forward to prepare myself for my first
+mary = User.create!(email: "mary@example.com", password: "123456", nickname: "Cool_Mary", bio: "Hi, I am Mary and I love coding. I want to do coding as a job. For my next interview I look for cool training partners.")
+mary.photo.attach(io: File.open(Rails.root.join('app','assets','images','mary.jpeg')), filename: 'mary.jpeg', content_type: 'image/jpeg')
+
+zoe = User.create!(email: "zoe@example.com", password: "123456", nickname: "zoe-bowie", bio: "I just finished my Computer Science degree and now I am looking for someone to practice with for a job interview.")
+zoe.photo.attach(io: File.open(Rails.root.join('app','assets','images','zoe.jpeg')), filename: 'zoe.jpeg', content_type: 'image/jpeg')
+
+peter = User.create!(email: "peter@example.com", password: "123456", nickname: "coding-king", bio: "I am the KING OF CODING and I will show you how it works so you get every job you want!")
+peter.photo.attach(io: File.open(Rails.root.join('app','assets','images','peter.jpeg')), filename: 'peter.jpeg', content_type: 'image/jpeg')
+
+lisa = User.create!(email: "lisa@example.com", password: "123456", nickname: "lizzy101", bio: "studied in England, worked as developer in Seoul, worked as senior develper in Tokio")
+lisa.photo.attach(io: File.open(Rails.root.join('app','assets','images','lisa.jpeg')), filename: 'lisa.jpeg', content_type: 'image/jpeg')
+
+bruce = User.create!(email: "bruce@example.com", password: "123456", nickname: "brucybruce", bio: "I got 10 years of experience in Ruby and Ruby on rails, CSS, JS and Python. I can help you prepare for your job interview!")
+bruce.photo.attach(io: File.open(Rails.root.join('app','assets','images','bruce.jpeg')), filename: 'bruce.jpeg', content_type: 'image/jpeg')
+
+rob = User.create!(email: "rob@example.com", password: "123456", nickname: "robbs", bio: "I got 10 years of experience in Ruby and Ruby on rails, CSS, JS and Python. I can help you prepare for your job interview!")
+rob.photo.attach(io: File.open(Rails.root.join('app','assets','images','rob.jpeg')), filename: 'rob.jpeg', content_type: 'image/jpeg')
+
+mario = User.create!(email: "mario@example.com", password: "123456", nickname: "mast2000", bio: "I am a strategy consultant based in Munich. I just finished the Le Wagon bootcamp in Munich to learn coding and acquire the right skillset to launch my own products!")
+mario.photo.attach(io: File.open(Rails.root.join('app','assets','images','mario.png')), filename: 'mario.png', content_type: 'image/png')
+
+nico = User.create!(email: "nico@example.com", password: "123456", nickname: "nicolaspck", bio: "I just finished school this year and decided to learn coding because I think that it will always be useful. Coding is now my passion and I am looking forward to prepare myself for my first
 job interview with like-minded companions on this plattform.")
-nico.photo.attach(io: file, filename: nico.nickname, content_type: 'image/jpg')
-nico.save!
+nico.photo.attach(io: File.open(Rails.root.join('app','assets','images','nico.jpg')), filename: 'nico.jpg', content_type: 'image/jepg')
+
 
 
 puts "generating Interviews"
